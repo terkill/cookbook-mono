ee2e03e5a84a1310d21dd388dbb3c2204e12a8c2
Guilhem Lettron <guilhem@lettron.fr>
2012-09-25 17:59:01 +0200

    typo

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Youscribe"
 maintainer_email "guilhem.lettron@youscribe.com"
-license          "Apache2"
+license          "Apache 2.0"
 description      "Installs/Configures mono"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
