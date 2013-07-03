9dd99c363ab1d174605c02addfac53f3f0a9a14b
Guilhem Lettron <guilhem@lettron.fr>
2013-07-03 17:30:43 +0200

    update version

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,5 @@ default['mono']['install_method'] = "packages"
 case platform
 when "debian","ubuntu"
   default['mono']['prefix']	=	"/usr/local"
-  default['mono']['branch']	=	"mono-3.0.0"
+  default['mono']['branch']	=	"mono-3.0.12"
 end
